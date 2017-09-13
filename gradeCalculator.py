9bff4973f93f7a64150f6d4c5716be272fd49115
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-09-13 13:52:22 -0400

    Updated from Brython Server: 9/13/2017 1:52:20 PM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,1 +1,16 @@
+#Clay Kynor
+#9/13/17
+#gradeCalculator.py - Calculates your letter grade
 
+grade = float(input("Enter your grade "))
+if grade>=90:
+    print("You earned an A! ")
+elif grade>=80:
+    print("You have a B ")
+elif grade>=70:
+    print("You have a C ")
+elif grade>=60:
+    print("You have a D ")
+else:
+    print("You have failed! ")
+    
