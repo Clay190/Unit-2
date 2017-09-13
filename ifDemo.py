356c156827805fde720dcedfb40c2a69aa8b1935
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-09-13 13:42:34 -0400

    Updated from Brython Server: 9/13/2017 1:42:32 PM

--- ifDemo.py
+++ ifDemo.py
@@ -1,1 +1,11 @@
+#Clay Kynor
+#9/13/17
+#ifDemo.py - how to use if statements
 
+num = float(input("Enter a number: "))
+if num>0:
+    print(num, "is possitive")
+elif num<0:
+    print(num, "is negative")
+else:
+    print(num, "is zero")
