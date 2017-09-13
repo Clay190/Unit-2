c8ce90c12036fdd834e6cdc28b0f7709ffb294b4
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-09-13 13:59:07 -0400

    Updated from Brython Server: 9/13/2017 1:59:05 PM

--- movie.py
+++ movie.py
@@ -1,1 +1,11 @@
+#Clay Kynor
+#9/13/17
+#movie.py - age into movie scadelous movie
 
+age = int(input("Enter your age "))
+if age>=17:
+    print("You may watch any movie you so choose, be careful though, there is some scary stuff out there! ")
+elif age>=13:
+    print("You can watch PG-13 movies, I recommend the Harry Potter series myself ")
+else:
+    print("You are restricted to only family friendly content ")
