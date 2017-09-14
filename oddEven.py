2a1a55dcfcfd46cf48fd09ee7e71fced85244df3
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-09-14 14:27:46 -0400

    Updated from Brython Server: 9/14/2017 2:27:45 PM

--- oddEven.py
+++ oddEven.py
@@ -1,1 +1,9 @@
+#Clay Kynor
+#9/14/17
+#oddEven.py - tells wether or not a number is even or odd
 
+num = float(input("Enter a number: "))
+if num%2 == 0:
+    print("Your number is even ")
+else:
+    print("Your number is odd ")
