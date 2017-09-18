82f766755e3d78c951d80a5c8ad2b35dee9b6e54
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-09-18 14:54:19 -0400

    Updated from Brython Server: 9/18/2017 2:54:18 PM

--- adventure.py
+++ adventure.py
@@ -1,1 +1,7 @@
+#Clay Kynor
+#9/18/17
+#adventure.py - Create an adventure
 
+answer = input("You are strolling along a field and see a wild Claire sitting on her most prized possesion, her bench. Do you approach her, yes or no?")
+if "yes" in answer:
+    print("You approach her and she leaps from the bench, eyes bulging, trying to defend her property, do you engage her in battle, yes or no?
