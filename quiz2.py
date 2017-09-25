c0bd46951f769092a951a0f8d613f3c581f853ea
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-09-25 14:23:55 -0400

    Updated from Brython Server: 9/25/2017 2:23:55 PM

--- quiz2.py
+++ quiz2.py
@@ -1,1 +1,13 @@
+#Clay Kynor
+#9/25/17
+#quiz2.py - my quiz
 
+num1 = float(input("Please enter a number: "))
+num2 = float(input("Please enter a second number: "))
+if num1>num2:
+    print("The first number is bigger")
+    if num1%3==0 and num2%3==0:
+        print("Both numbers are divisible by 3")
+        prod1 = int(input("What is the product of your numbers?"))
+        if (num1*num2):
+            print("Correct")
