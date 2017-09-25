4255209b3665f156ae4a3bd2d4c8f32b566fc788
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-09-25 14:34:05 -0400

    Updated from Brython Server: 9/25/2017 2:34:05 PM

--- quiz2.py
+++ quiz2.py
@@ -9,5 +9,74 @@ if num1>num2:
     if num1%3==0 and num2%3==0:
         print("Both numbers are divisible by 3")
         prod1 = int(input("What is the product of your numbers?"))
-        if (num1*num2):
+        if (num1*num2)==prod1:
             print("Correct")
+        else:
+            print("Incorrect")
+    elif num1%3==0:
+        print("Only the first number is divisible by 3")
+        prod2 = int(input("What is the product of your numbers?"))
+        if (num1*num2)==prod2:
+            print("Correct")
+        else:
+            print("Incorrect")
+    elif num2%3==0:
+        print("Only the second number is divisible by 3")
+        prod3 = int(input("What is the product of your numbers?"))
+        if (num1*num2)==prod3:
+            print("Correct")
+        else:
+            print("Incorrect")
+    else:
+        print("None of you numbers are divisible by 3")
+        prod4 = int(input("What is the product of your numbers?"))
+        if (num1*num2)==prod4:
+            print("Correct")
+        else:
+            print("Incorrect")
+elif num1<num2:
+    print("The second number is bigger")
+    if num1%3==0 and num2%3==0:
+        print("Both numbers are divisible by 3")
+        pro1 = int(input("What is the product of your numbers?"))
+        if (num1*num2)==pro1:
+            print("Correct")
+        else:
+            print("Incorrect")
+    elif num1%3==0:
+        print("Only the first number is divisible by 3")
+        pro2 = int(input("What is the product of your numbers?"))
+        if (num1*num2)==pro2:
+            print("Correct")
+        else:
+            print("Incorrect")
+    elif num2%3==0:
+        print("Only the second number is divisible by 3")
+        pro3 = int(input("What is the product of your numbers?"))
+        if (num1*num2)==pro3:
+            print("Correct")
+        else:
+            print("Incorrect")
+    else:
+        print("None of you numbers are divisible by 3")
+        pro4 = int(input("What is the product of your numbers?"))
+        if (num1*num2)==pro4:
+            print("Correct")
+        else:
+            print("Incorrect")
+else:
+    print("Your number are the same")
+    if num1%3==0:
+        print("Both your numbers are divisible by 3")
+        product1 = int(input("What is the product of your numbers?"))
+        if num1**2==product1:
+            print("Correct")
+        else:
+            print("False")
+    else:
+        print("Your numbers are not divisible by 3")
+        product2 = int(input("What is the product of your numbers?"))
+        if num1**2==product2:
+            print("Correct")
+        else:
+            print("False")
